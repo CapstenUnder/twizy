5b4be2aa3f8b0c9c58dc2ed9fb1bc5356b20efb9
Sam Nehme <samneme55@gmail.com>
2020-05-04 10:18:00 +0200

    updated gps_calc to most recent version and removed gps_calc2 and 3

--- scripts/gps_calc.py
+++ scripts/gps_calc.py
@@ -1,144 +1,119 @@
 #!/usr/bin/env python
-import math
 import numpy as np
 import rospy
-import os
-import errno
 import utm
 from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import String
+import matplotlib.pyplot as plt
+
 
 msg_to_publish = Float32MultiArray()
+angle = 0
+gps_v_x, gps_h_x, gps_v_y, gps_h_y = 0, 0, 0, 0
+x_ref, y_ref, angle_ref = 0, 0, 0
+run_once = 0
 
-global qr
-qr = 0
 
+# sets the reference point.
 def get_ref():
+    global gps_v_x, gps_v_y
+    global x_ref, y_ref, angle_ref
     x_ref = gps_v_x
     y_ref = gps_v_y
-    angle_ref = angle
+    angle_ref = 2*np.pi - angle
 
-def calcangle():
-     global angle
-     gps_x_diff = gps_v_x - gps_h_x
-     gps_y_diff = gps_v_y - gps_h_y
-     #print( gps_x_diff)
-     #print( gps_y_diff)
-     angle = np.arctan2(gps_y_diff, gps_x_diff) + np.pi
-     #print([gps_x_diff, gps_y_diff, np.degrees(angle)])
+
+def calc_angle():
+    global angle
+    global gps_v_x, gps_v_y, gps_h_x, gps_h_y
+    gps_x_diff = gps_v_x - gps_h_x
+    gps_y_diff = gps_v_y - gps_h_y
+    angle = np.arctan2(gps_y_diff, gps_x_diff) + np.pi
+    # print([gps_x_diff, gps_y_diff, np.degrees(angle)])
+
 
 def talker():
-    pub = rospy.Publisher('GPS_pos', Float32MultiArray , queue_size=2)
+    global angle
+    global x_ref, y_ref, angle_ref
+
+    pub = rospy.Publisher('GPS_pos', Float32MultiArray, queue_size=2)
     while not rospy.is_shutdown():
-        msg_to_publish.data = [gps_v_x, gps_v_y, angle]
-        pub.publish(msg_to_publish)
-	print([gps_v_x, gps_v_y, angle])
-	break
+
+        # moves and rotates the global coordinates to local coordinate determined by the reference point and angle
+        x = gps_v_x - x_ref
+        y = gps_v_y - y_ref
+
+
+        local_x = x * np.cos(angle_ref) - y * np.sin(angle_ref)
+        local_y = x * np.sin(angle_ref) + y * np.cos(angle_ref)
+        local_angle = (angle + angle_ref) % (2*np.pi)
+
+        msg_to_publish.data = [local_x, local_y, local_angle]
+	#msg_to_publish.data = [x, y, angle]
+	pub.publish(msg_to_publish)
+
+	"""
+	# For plotting gps coordinates on xy-axles
+	size = 10
+	plt.scatter(x, y, color="red")
+	plt.scatter(local_x, local_y, color="blue")
+	plt.axis([-size, size, -size, size])
+	plt.ion()
+	plt.show()
+	plt.pause(0.01)
+
+	"""
+	print([local_x, local_y, np.degrees(local_angle), np.degrees(angle), angle_ref])
+        #print([gps_v_x, gps_v_y, angle])
+        break
+
 
 def callback_h(data):
     gps_string_h = data.data
-    GPS_h = gps_string_h.split(",")
-    GPS_h_lon = float(GPS_h[0])
-    GPS_h_lat = float(GPS_h[1])
+    gps_h = gps_string_h.split(",")
+    gps_h_lon = float(gps_h[0])
+    gps_h_lat = float(gps_h[1])
 
-    long_start = 11.9776858793
-    lat_start = 57.6884871703
-
-    r_earth = 6371000
-    
- 
     global gps_v_x
     global gps_v_y
-    #gps_v_x = r_earth * math.sin(90-lat_start*math.pi/180) * GPS_h_lon*math.pi/180 
-    #gps_v_y = r_earth * GPS_h_lat*math.pi/180
-    #test utm
-    gps_v_pos = utm.from_latlon(GPS_h_lat,GPS_h_lon)
+
+    gps_v_pos = utm.from_latlon(gps_h_lat, gps_h_lon)
     gps_v_x = gps_v_pos[0]
     gps_v_y = gps_v_pos[1]
-    #print(data.data)
-    #print(GPS_h_lon)
-    #print(GPS_h_lat)
-    
-   
-   
-    calcangle()
-   
+    calc_angle()
+
+    global run_once
+    if run_once < 2:
+        run_once += 1
+    elif run_once == 2:
+	get_ref()
+	run_once += 1
+
     talker()
+
 
 def callback_v(data):
     gps_string = data.data
-    GPS_v = gps_string.split(",")
-    GPS_v_lon = float(GPS_v[0])
-    GPS_v_lat = float(GPS_v[1])
+    gps_v = gps_string.split(",")
+    gps_v_lon = float(gps_v[0])
+    gps_v_lat = float(gps_v[1])
 
-    long_start = 11.9776858793
-    lat_start = 57.6884871703
-
-    r_earth = 6371000
- 
     global gps_h_x
     global gps_h_y
-    #gps_h_x = r_earth * math.sin(90-lat_start*math.pi/180) * GPS_v_lon*math.pi/180 
-    #gps_h_y = r_earth * GPS_v_lat*math.pi/180 
-    
-    gps_h_pos = utm.from_latlon(GPS_v_lat,GPS_v_lon)
-    gps_h_x = gps_h_pos[0] 
-    gps_h_y = gps_h_pos[1]
-    global qr
-    
-    qr = 1
-    
-#test
-#def callback_test(data):
-#    gps_pooos = data.data
-#    global gps_h_x
-#    global gps_h_y
-#    global gps_v_x
-#    global gps_v_y
-#    global qr
-#    qr = 1
-#    gps_h_x = gps_pooos[2]
-#    gps_h_y = gps_pooos[3]
-#    gps_v_x = gps_pooos[0]
-#    gps_v_y = gps_pooos[1]
-#    calcangle()
-   
-#    talker()
-    
-    
-def listener():
-    #FIFO = "/tmp/posdata"
-    #try:
-    #    os.mkfifo(FIFO)
-    #except OSError as oe: 
-    #    if oe.errno != errno.EEXIST:
-    #        raise
 
-    #print("Opening FIFO...")
-    #with open(FIFO) as fifo:
-    #    print("FIFO opened")
-    #    while True:
-    #        data = fifo.read()
-    #        if len(data) == 0:
-    #            print("Writer closed")
-    #            break
-    #        print('Read: "{0}"'.format(data))
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
+    gps_h_pos = utm.from_latlon(gps_v_lat, gps_v_lon)
+    gps_h_x = gps_h_pos[0]
+    gps_h_y = gps_h_pos[1]
+
+
+def listener():
     rospy.init_node('GPS_calc', anonymous=True)
 
-    #test
-    #rospy.Subscriber('GPS_pos_fake', Float32MultiArray , callback_test)
-    
-  
     rospy.Subscriber('GPS_left', String, callback_v, queue_size=1)
     rospy.Subscriber('GPS_right', String, callback_h, queue_size=1)
 
-    #spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()

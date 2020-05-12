3fec72c1653b4ba4a8476f0eed7fe001b1ce6138
Sam <samneme55@gmail.com>
2020-05-12 13:49:14 +0200

    implemented parking map into pure pursuit code

--- scripts/pure_pursuit_wrapper.py
+++ scripts/pure_pursuit_wrapper.py
@@ -7,6 +7,39 @@ from matplotlib import pyplot as plt
 import numpy as np
 import math
 
+class Map:
+    def __init__(self,coordstart, coordbreak1, coordbreak2, coordend, twizydist, offset, GPS_y):
+        self.coordstart = coordstart
+        self.coordbreak1 = coordbreak1
+        self.coordbreak2 = coordbreak2
+        self.coordend = coordend
+        self.twizydist = twizydist
+        self.offset = offset
+        self.GPS_y = GPS_y
+
+    # all coordinates come from the GPS, twizydist from rear ultrasonic sensor
+    def generateMap(self):
+        offset_length = np.linspace(self.coordstart, self.coordbreak1 - 0.001, 20)
+        extradots = np.linspace(self.coordbreak1, self.coordbreak1 + 0.1, 20)
+        parkingspot_length = np.linspace(self.coordbreak1 + 0.1, self.coordbreak2, 20)
+        end_length = np.linspace(self.coordbreak2, self.coordend, 20)
+
+        parkingspotdepth = 2.5
+        #  y = 25x + (distance . 25*offset)
+        parkmap = {}
+
+        for x in offset_length:
+            parkmap[x] = self.twizydist + self.GPS_y
+        for x in extradots:
+            parkmap[x] = 25 * x + (self.twizydist - 25 * self.offset) + self.GPS_y
+        for x in parkingspot_length:
+            parkmap[x] = parkingspotdepth + self.twizydist + self.GPS_y
+        for x in end_length:
+            parkmap[x] = self.twizydist + self.GPS_y
+
+        return parkmap
+
+
 class Wrapper:
 
     def __init__(self):
@@ -22,7 +55,7 @@ class Wrapper:
         self.collision_warning = False
 	self.stop = False
 	self.target_ind = None
-
+        self.parking_map = {}
 
 
     def shutdown_hook(self):
@@ -91,7 +124,9 @@ class Wrapper:
                 print(angle,target_speed)
                 pub.publish(msg_to_publish)
                 self.ros_plot(states)
-                #rospy.on_shutdown(self.shutdown_hook())
+                plt.plot(list(self.parking_length.keys()), list(self.parking_map.values()), 'black')
+
+        #rospy.on_shutdown(self.shutdown_hook())
 
 		self.stop = True
 		exit()
@@ -109,7 +144,15 @@ class Wrapper:
             self.counter += 1
 	    state.update_from_gps(self.GPS , -1)
 	    self.target_ind, _ = path.search_target_index(state)
-	    self.path_is_ready = True
+
+        #parameters for parking map
+            start = 0
+            p1 = self.offset + self.GPS[0]
+            p2 = p1 + self.parking_ength
+            end = p2 + 3
+            self.parking_map = Map(start, p1, p2, end, self.distance, self.offset, self.GPS[1]).generateMap()
+
+            self.path_is_ready = True
 
 
     def ultrasonic_callback(self,msg):
